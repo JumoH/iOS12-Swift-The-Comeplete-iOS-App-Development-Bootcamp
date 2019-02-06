25ab7428d0da6d3f5c33c9d91150a3391c99a1bb
Junmo Han <wnsah052@naver.com>
2019-02-06 22:44:31 +0900

    190206

---
+++ Grammar/Comments, Printing and the Console.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var monsterHealth = 19
+
+monsterHealth += 22
+
+monsterHealth /= 2
+
+print(monsterHealth)
+print("Hello World")
---
+++ Grammar/Functions.playground/Contents.swift
@@ -0,0 +1,11 @@
+// Creating the getMilk() function
+func getMilk() {
+    print("go to the shops")
+    print("buy 2 cartons of milk")
+    print("pay $2")
+    print("come home")
+}
+
+// Calling the getMilk() function
+getMilk()
+
