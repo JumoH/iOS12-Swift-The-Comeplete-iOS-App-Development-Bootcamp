ab6736580bd2484d6a874b327f284dd3a062b312
Junmo Han <wnsah052@naver.com>
2019-02-07 01:22:37 +0900

    190207

--- Grammar/Functions.playground/Contents.swift
+++ Grammar/Functions.playground/Contents.swift
@@ -1,11 +1,23 @@
 // Creating the getMilk() function
-func getMilk() {
+//func getMilk() {
+//    print("go to the shops")
+//    print("buy 2 cartons of milk")
+//    print("pay $2")
+//    print("come home")
+//}
+
+func getMilk(howManyMilkCartons: Int) {
+    
+    
     print("go to the shops")
-    print("buy 2 cartons of milk")
-    print("pay $2")
+    print("buy \(howManyMilkCartons) cartons of milk")
+
+    let priceToPay = howManyMilkCartons * 2
+
+    print("pay $\(priceToPay)")
     print("come home")
 }
 
 // Calling the getMilk() function
-getMilk()
+getMilk(howManyMilkCartons: 4)
 
